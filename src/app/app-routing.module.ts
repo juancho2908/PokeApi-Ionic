962f6d93c247cd7a2a4ae3fb515fefda5f95d591
juancho2908 <juanchercuelon@gmail.com>
2021-02-10 13:55:59 -0500

    version1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,13 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+ 
+  {
+    //La pagina de detalles esta bien pero lo recomendable si vas a una pagina de detalles seria pasar un id para que pueda cargar la información 
+    //En mi caso, muevo la página de home a esta página y la llamo index y asi pasar el index del pokemon para que luego podamos recuperar la información de esa página.
+    path: 'home/:index',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule) //aqui le digo que en index me presente los detalles de cada pokemon y que me permita obtener el nombre y la imagen que aqui se alojan.
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import {PokemonService} from './../services/pokemon.service'; //importo el pokemon Service
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  offset=0
+  pokemon=[];
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;//Este parametro permite seguir cargando mas datos y los presenta
+  constructor(private pokeService: PokemonService) { }
 
-  constructor() {}
+  ngOnInit() {
+    this.loadPokemon(); // Envio el parametro de load pokemon
+  }
 
+  // El load pokemon obtendra los datos del pokemonSevice
+  loadPokemon(loadMore = false, event?){
+    if(loadMore){
+      this.offset+=25; //Aqui le digo que por medio de un evento me cargue mas datos pero esta vez ya no inicia de 0 si no desde el limite 25
+    }
+    this.pokeService.getPokemon(this.offset).subscribe(res =>{
+        this.pokemon = [...this.pokemon, ...res];
+
+        if (event){
+          event.target.complete(); //Finalmente me completa los datos
+        }
+
+        if(this.offset ==125){
+          this.infinite.disabled = true; // Aqui le digo que si la funcion llega hasta 125 me desahabilite los demas pokemons.
+        }
+    });
+
+  }
+  onSearchChange(e){ //Esta funcion es para buscar los pokemons
+    let value =e.detail.value;
+    if (value ==''){
+      this.offset =0;
+      this.loadPokemon();
+      return;
+    }
+  }
 }
